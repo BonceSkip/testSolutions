f03e7c7fd7041b34908df9f5ed7ae5e51990de7f
charlesrv <charlesrv00@gmail.com>
2021-05-10 11:34:04 -0400

    completando código para soluciones

--- src/main/java/SolucionA.java
+++ src/main/java/SolucionA.java
@@ -1,10 +1,27 @@
 public class SolucionA {
+	final static char U = 'U';
+	final static char D = 'D';
 
     // Complete the countingValleys function below.
     static int countingValleys(int n, String s) {
         int valleys = 0;
-
+        int altitude = 0;
+        boolean enterV = false;
+        
+        for(int i = 0; i < n ; i++) { 
+            char c = s.charAt(i);
+            
+            if (c == U) altitude++;
+            else if (c == D) altitude--;
+            
+            if (altitude < 0 && !enterV) {
+            	enterV = true;
+            	valleys++;
+            } else if (altitude >= 0) {
+            	enterV = false;
+            }
+        }
+        
         return valleys;
-
     }
 }
--- src/main/java/SolucionB.java
+++ src/main/java/SolucionB.java
@@ -1,8 +1,32 @@
 public class SolucionB {
+	final static char A = 'a';
 
     // Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
         long resultado = 0;
+    	int len = s.length();
+        
+    	if (len >= n) {
+    		for (int i = 0; i < n; i++) {
+    			if (s.charAt(i) == A) {
+                	resultado++;
+                }
+    		}
+    	} else {
+    		long times = n / len;
+
+    		for (int i = 0; i < len; i++) {
+    			if (s.charAt(i) == A) {
+                	resultado++;
+                }
+    		}
+    		resultado *= times;
+            for (int i = 0; i < n % len; i++) {
+                if (s.charAt(i) == A) {
+                	resultado++;
+                }
+            }
+    	}
 
         return resultado;
     }
